ad82fce6a9c47ee2ef6395cca5d6e13912d87e53
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2024-06-29 23:38:31 +0530

    removal of asynch implementation

--- back_end/Utils/mongo_utils.py
+++ back_end/Utils/mongo_utils.py
@@ -1,26 +1,19 @@
 import os
 import motor.motor_asyncio
+from pymongo import MongoClient
 
+# MongoDB Utils Class
 class MongoUtils:
     def __init__(self):
         self.cached_db = None
+
     def connect_to_database(self):
-        ## user data
         if self.cached_db:
             return self.cached_db
-        MONGO_URI = "mongodb+srv://dev:BsOd6D1sWfgcLKhA@dealdex.3myg7vw.mongodb.net/?retryWrites=true&w=majority&appName=DealDex"
-        # Fetch MongoDB URI from environment variable
-        mongodb_uri = MONGO_URI
-        # mongodb_uri = os.environ.get('MONGO_URI')
-
-        if not mongodb_uri:
+        MONGO_URI = "mongodb+srv://anirudh:eFIXfhVD7wNTUCCr@dealdex.3myg7vw.mongodb.net/?retryWrites=true&w=majority&appName=DealDex"
+        if not MONGO_URI:
             raise ValueError("MongoDB URI not found in environment variables.")
-
-        # Connect to MongoDB using the fetched URI
-        client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_uri)
-
-        # Specify database
+        client = MongoClient(MONGO_URI)
         db = client.DealDex
-
         self.cached_db = db
         return db
