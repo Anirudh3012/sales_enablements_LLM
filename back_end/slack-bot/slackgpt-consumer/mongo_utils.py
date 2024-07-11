83306b75ce20a07d0f0bd175a3e8d6ef95948f20
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2024-07-11 11:00:34 +0530

    final changes to consumer

--- back_end/slack-bot/slackgpt-consumer/mongo_utils.py
+++ back_end/slack-bot/slackgpt-consumer/mongo_utils.py
@@ -9,12 +9,13 @@ class MongoUtils:
         self.cached_db = None
 
     async def connect_to_database(self):
-        if self.cached_db:
-            return self.cached_db
-        MONGO_URI = "mongodb+srv://anirudh:eFIXfhVD7wNTUCCr@dealdex.3myg7vw.mongodb.net/?retryWrites=true&w=majority&appName=DealDex"
-        if not MONGO_URI:
-            raise ValueError("MongoDB URI not found in environment variables.")
-        client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-        db = client.DealDex
-        self.cached_db = db
-        return db
+        if self.cached_db is None:
+
+            MONGO_URI = "mongodb+srv://anirudh:eFIXfhVD7wNTUCCr@dealdex.3myg7vw.mongodb.net/?retryWrites=true&w=majority&appName=DealDex"
+            if not MONGO_URI:
+                raise ValueError("MongoDB URI not found in environment variables.")
+            client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+            db = client.DealDex
+            self.cached_db = db
+            return db
+        return self.cached_db
